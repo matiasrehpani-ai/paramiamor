84c3a735e1d6fdce7e62952e68149fc71e1bc20a
matiasrehpani-ai <matias.rehpani@gmail.com>
2026-04-07 02:09:05 -0400

    primer commit

---
+++ letter.js
@@ -0,0 +1,19 @@
+const LETTER = {
+  date: "Quito, abril 2026",
+
+  text: `Mi vida,
+
+No sé que tan bien salga este proyecto, pero si sale como lo estoy pensando es algo realmente hermoso que estoy seguro que le va a encantar. En verdad quiero agradecerle por ser la niña más hermosa y más preciosa del planeta entero. De verdad que me siento como el hombre más afortunado por tenerte y quiero que así sea por el resto de nuestros días vida de mi vida.
+
+En este pequeño lugar va a poder encontrar muchas cosas, como un mapa de los lugares más importantes de nuestra relación, esta linda carta, una ruleta en donde podremos elegir diferentes planes para nuestro verano, un álbum de fotos y algunas de las pocas razones por las cuales le amo y le adoro. Si escribiera todas se me acabaría el espacio juas juas juas.
+
+Realmente no soy ningún experto ni nada por el estilo pero tengo en mente como va a salir esto y estoy muy emocionado. Hacer un rato usted estaba brava y ahorita estoy trabajando en el detalle que más me ha emocionado durante nuestra relación.
+
+En verdad sé que esto nos va a durar toda la vida y que juntos vamos a construir ese futuro que tanto nos emociona y que tan felizmente vamos a vivir, porque nos queda una vida juntos, un futuro lleno de sonrisas, de recuerdos, de familia, de hijos, de todo mi amor.
+
+Estoy seguro que eres el amor de mi vida y que te quiero tener a mi lado todos los días de mi vida. En verdad espero que disfrutes esto y me cuentes que tal.
+
+Con todo mi amor,`,
+
+  signature: "Matías 🤍"
+};
